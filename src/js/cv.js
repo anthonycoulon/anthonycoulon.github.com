7624ddb03ef215e0a3a0666ef70639c149dd44c5
Anthony <coulon.anthony@gmail.com>
2015-03-08 10:33:54 +0100

    continue cv interface : add social logo, improve css effect, start responsive design

--- src/js/cv.js
+++ src/js/cv.js
@@ -1,8 +1,8 @@
 Cv = function() {};
 
 Cv.prototype.animate = function() {
-	$('header').css('height', $(window).height());
-	var heightBeforeSlides=$('header').height()+$('#about-me').height();
+	$('header.home').css('height', $(window).height());
+	var heightBeforeSlides=$('header.home').height()+$('#about-me').height();
 	var nbElemShown=0;
 	var that=this;
 
@@ -11,16 +11,31 @@ Cv.prototype.animate = function() {
 	var slides = $('.slide');
 	$(window).scroll(function() {
 
-		if(_ret.heightSlides<$(window).scrollTop()+$(window).height()) {
-			_ret.heightSlides+=$(slides[_ret.nbElemShown]).height();
-			$(slides[_ret.nbElemShown]).addClass('slide-anim');
-			_ret.nbElemShown++;
-			console.log(_ret.nbElemShown);
-		}
-		if($(window).scrollTop()==0) {
-			$('.slide').removeClass('slide-anim');
-			_ret = that.animSlideByHeight(heightBeforeSlides, nbElemShown);
-		}
+        if($(window).scrollTop()>$('header.home').height()/5) {
+            $('#about-me').addClass('no-blur');
+            $('header.home').addClass('blur');
+        }else {
+            $('header.home').removeClass('blur');
+        }
+
+        if(_ret.heightSlides<$(window).scrollTop()+$(window).height()) {
+            _ret.heightSlides+=$(slides[_ret.nbElemShown]).height();
+            $(slides[_ret.nbElemShown]).addClass('slide-anim');
+            _ret.nbElemShown++;
+            console.log(_ret.nbElemShown);
+        }
+
+        if($(window).scrollTop()>$('header.home').height()) {
+            $('.header-nav').addClass('nav-show');
+        }else {
+            $('.header-nav').removeClass('nav-show');
+        }
+
+        if($(window).scrollTop()==0) {
+            $('.slide').removeClass('slide-anim');
+            _ret = that.animSlideByHeight(heightBeforeSlides, nbElemShown);
+            $('#about-me').removeClass('no-blur');
+        }
 	});
 };
 
